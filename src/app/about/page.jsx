6f7c2335ba56549deda177696118be710b7b5f45
Yusuf Kaplan <yusufkaplan3591@gmail.com>
2024-04-13 00:56:40 +0300

    day1

---
+++ src/app/about/page.jsx
@@ -0,0 +1,29 @@
+import Link from 'next/link'
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>AboutPage</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos amet ducimus odio iure expedita similique eaque, quasi quo culpa excepturi necessitatibus, esse consectetur sed aperiam veritatis mollitia rerum ab repellat?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos amet ducimus odio iure expedita similique eaque, quasi quo culpa excepturi necessitatibus, esse consectetur sed aperiam veritatis mollitia rerum ab repellat?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos amet ducimus odio iure expedita similique eaque, quasi quo culpa excepturi necessitatibus, esse consectetur sed aperiam veritatis mollitia rerum ab repellat?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos amet ducimus odio iure expedita similique eaque, quasi quo culpa excepturi necessitatibus, esse consectetur sed aperiam veritatis mollitia rerum ab repellat?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos amet ducimus odio iure expedita similique eaque, quasi quo culpa excepturi necessitatibus, esse consectetur sed aperiam veritatis mollitia rerum ab repellat?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos amet ducimus odio iure expedita similique eaque, quasi quo culpa excepturi necessitatibus, esse consectetur sed aperiam veritatis mollitia rerum ab repellat?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos amet ducimus odio iure expedita similique eaque, quasi quo culpa excepturi necessitatibus, esse consectetur sed aperiam veritatis mollitia rerum ab repellat?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos amet ducimus odio iure expedita similique eaque, quasi quo culpa excepturi necessitatibus, esse consectetur sed aperiam veritatis mollitia rerum ab repellat?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos amet ducimus odio iure expedita similique eaque, quasi quo culpa excepturi necessitatibus, esse consectetur sed aperiam veritatis mollitia rerum ab repellat?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos amet ducimus odio iure expedita similique eaque, quasi quo culpa excepturi necessitatibus, esse consectetur sed aperiam veritatis mollitia rerum ab repellat?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos amet ducimus odio iure expedita similique eaque, quasi quo culpa excepturi necessitatibus, esse consectetur sed aperiam veritatis mollitia rerum ab repellat?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos amet ducimus odio iure expedita similique eaque, quasi quo culpa excepturi necessitatibus, esse consectetur sed aperiam veritatis mollitia rerum ab repellat?</p>
+      
+
+      <Link href="/contact" scroll={false} >Go to contact page</Link><br />
+      <Link href="/products?id=23">Product</Link>
+
+    </div>
+  )
+}
+
+export default AboutPage
---
+++ src/app/contact/page.jsx
@@ -0,0 +1,29 @@
+import Link from 'next/link'
+import React from 'react'
+
+const ContactPage
+ = () => {
+  return (
+    <div>
+      <h1>ContactPage</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos amet ducimus odio iure expedita similique eaque, quasi quo culpa excepturi necessitatibus, esse consectetur sed aperiam veritatis mollitia rerum ab repellat?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos amet ducimus odio iure expedita similique eaque, quasi quo culpa excepturi necessitatibus, esse consectetur sed aperiam veritatis mollitia rerum ab repellat?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos amet ducimus odio iure expedita similique eaque, quasi quo culpa excepturi necessitatibus, esse consectetur sed aperiam veritatis mollitia rerum ab repellat?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos amet ducimus odio iure expedita similique eaque, quasi quo culpa excepturi necessitatibus, esse consectetur sed aperiam veritatis mollitia rerum ab repellat?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos amet ducimus odio iure expedita similique eaque, quasi quo culpa excepturi necessitatibus, esse consectetur sed aperiam veritatis mollitia rerum ab repellat?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos amet ducimus odio iure expedita similique eaque, quasi quo culpa excepturi necessitatibus, esse consectetur sed aperiam veritatis mollitia rerum ab repellat?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos amet ducimus odio iure expedita similique eaque, quasi quo culpa excepturi necessitatibus, esse consectetur sed aperiam veritatis mollitia rerum ab repellat?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos amet ducimus odio iure expedita similique eaque, quasi quo culpa excepturi necessitatibus, esse consectetur sed aperiam veritatis mollitia rerum ab repellat?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos amet ducimus odio iure expedita similique eaque, quasi quo culpa excepturi necessitatibus, esse consectetur sed aperiam veritatis mollitia rerum ab repellat?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos amet ducimus odio iure expedita similique eaque, quasi quo culpa excepturi necessitatibus, esse consectetur sed aperiam veritatis mollitia rerum ab repellat?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos amet ducimus odio iure expedita similique eaque, quasi quo culpa excepturi necessitatibus, esse consectetur sed aperiam veritatis mollitia rerum ab repellat?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos amet ducimus odio iure expedita similique eaque, quasi quo culpa excepturi necessitatibus, esse consectetur sed aperiam veritatis mollitia rerum ab repellat?</p>  
+
+      <Link href="/about">Go to about page</Link>
+
+    </div>
+
+  )
+}
+
+export default ContactPage
